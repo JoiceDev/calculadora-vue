e3dd42d7fa8fce4a40a08f97f6e0973c840101ab
Joice Gonçalves <joice.silva.s2@hotmail.com>
2024-11-30 12:19:06 -0300

    Changing the styling and conditional visibility for the result.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="centro">
-    <h1>Calculadora VueJS</h1>
-    <input v-model.number="estado.num1" @keyup="calcular">
-    <select v-model="estado.operação" @change="calcular">
-      <option value="+">+</option>
-      <option value="-">-</option>
-      <option value="*">x</option>
-      <option value="/">/</option>
-    </select>
-    <input v-model.number="estado.num2" @keyup="calcular">
-    <span>Resultado: {{ estado.result }}</span>
+    <div class="caixa">
+      <h1>Calculadora VueJS</h1>
+      <input v-model.number="estado.num1" @keyup="calcular" placeholder="Número 1">
+      <select v-model="estado.operação" @change="calcular">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">x</option>
+        <option value="/">/</option>
+      </select>
+      <input v-model.number="estado.num2" @keyup="calcular" placeholder="Número 2">
+      <div class="resultado" v-if="estado.result !== null">
+        <span>Resultado: {{ estado.result }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,6 +28,11 @@ const estado = reactive({
 });
 
 const calcular = () => {
+  if (estado.operação === '/' && estado.num2 === 0) {
+    estado.result = 'Erro! Divisão por zero';
+    return;
+  }
+  
   switch (estado.operação) {
     case '+':
       estado.result = estado.num1 + estado.num2;
@@ -45,7 +54,6 @@ const calcular = () => {
 </script>
 
 <style scoped>
-
 .centro {
   display: flex;
   flex-direction: column;
@@ -53,17 +61,48 @@ const calcular = () => {
   justify-content: center;
   height: 100vh; 
   text-align: center;
-  background-color: rgba(144, 238, 144, 0.758);
+  background-color: rgba(144, 238, 144, 0.567);
+}
+.caixa {
+  padding: 20px;
+  background-color: rgba(255, 255, 255, 0); 
+  border: 2px solid rgba(1, 34, 12, 0.3); 
+  border-radius: 10px; 
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.8); 
 }
 
 input, select {
-  padding: 4px;
-  width: 50px;
-  margin: 6px;
+  padding: 10px;  
+  width: 120px;
+  margin: 10px;
+  font-size: 16px;
+  background-color: rgba(255, 255, 255, 0);
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(2, 37, 24, 0.7);
+
+}
+
+input:focus, select:focus {
+  outline: none;
+  box-shadow: 0 0 10px rgba(2, 37, 24, 0.7);
+}
+
+.resultado {
+  margin-top: 20px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  ;
 }
 
 span {
   font-weight: bold;
-  margin: 8px;
+  margin: 12px;
+  font-size: 18px;
+  color: #000000;
+}
+
+h1 {
+  margin-bottom: 20px;
 }
 </style>
+
